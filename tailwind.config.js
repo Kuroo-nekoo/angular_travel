00fc48ad68c5154d4e7dc5088405b6b3ca41b0c5
Tri Hung <73674485+Kuroo-nekoo@users.noreply.github.com>
2023-02-20 00:34:59 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#ff5115",
+        },
+      },
+    ],
+  },
+};
